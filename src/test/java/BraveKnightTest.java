9eb3c4e6a1adb8a05598c76abb71f35dcfaf91e5
Wiktor Kalinowski <wiktorkalinowski@gmail.com>
2019-11-21 23:06:20 +0100

    junit5 @ExtendWith mock, injectMock

--- src/test/java/BraveKnightTest.java
+++ src/test/java/BraveKnightTest.java
@@ -1,13 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class BraveKnightTest {
 
+    @InjectMocks
+    private BraveKnight braveKnight;
+    @Mock
+    private Quest mockQuest;
+
     @Test
-    public void embarkOnQuest() {
-        Quest mockQuest = mock(Quest.class);
-        BraveKnight braveKnight = new BraveKnight(mockQuest);
+    void embarkOnQuest() {
         braveKnight.embarkOnQuest();
         verify(mockQuest, times(1)).embark();
     }
